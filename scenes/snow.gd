39bf95bb495616ae686dea7b4f87353cfea1f681
deweyhinni <deweyhinni@protonmail.com>
2025-01-14 23:51:26 +0000

    snow shader displacement and stuff

--- scenes/snow.gd
+++ scenes/snow.gd
@@ -1,10 +1,39 @@
 extends MeshInstance3D
 
+@onready var player: CharacterBody3D = $"../Player"
+
+var shader_mat: ShaderMaterial;
+var path_map_texture: ImageTexture;
+var path_image: Image = Image.create(1000, 1000, false, Image.FORMAT_RGB8);
+
+var last_time: float = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	shader_mat = self.get_active_material(0)
+	shader_mat.set_shader_parameter("path_map", ImageTexture.create_from_image(path_image))
+	last_time = Time.get_ticks_msec()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	path_map_texture = shader_mat.get_shader_parameter("path_map")
+	var player_relative_pos: Vector2 = Vector2(player.position.x - position.x, player.position.z - position.z)
+	var image_coord = Vector2i((player_relative_pos * 10.0).x + 500.0, (player_relative_pos * 10.0).y + 500.0)
+	var brush_size = 5
+	if (image_coord.x < 1000 && image_coord.y < 1000 && image_coord.x > 0 && image_coord.y > 0 && player.is_on_floor()):
+		for x in range(-brush_size, brush_size):
+			for y in range(-brush_size, brush_size):
+				if (image_coord.x + x < 1000 && image_coord.y + y < 1000 && image_coord.x + x > 0 && image_coord.y + y > 0):
+					var from_center: float = sqrt(pow(abs(x),2) + pow(abs(y),2))
+					if from_center <= brush_size:
+						var color: Color = Color(1.0 - (from_center/float(brush_size)), 0.0,0.0)
+						path_image.set_pixelv(image_coord + Vector2i(x,y), color)
+	if Time.get_ticks_msec() - last_time > 10.0:
+		path_map_texture.update(path_image)
+		last_time = Time.get_ticks_msec()
+	if Input.is_key_pressed(KEY_F2):
+		path_image.save_exr("path_map.exr", false)
+	
+	
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -28,6 +28,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton && Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -36,6 +37,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
+	
+	if Input.is_action_pressed("run"):
+		speed = run_speed
+	else:
+		speed = walk_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
