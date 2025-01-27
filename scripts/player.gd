7254d20cbb811e2fa1ec3b963edf717e65717d5c
hinrikfp <hinrikfp06@gmail.com>
2025-01-27 00:48:35 +0000

    basic climbing functionality for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,7 +22,19 @@ extends CharacterBody3D
 
 var speed: float = walk_speed;
 
-func can_climb() -> bool:
+enum PlayerMovement {
+	Idle,
+	Walking,
+	Running,
+	Jumping,
+	Falling,
+	Sliding,
+	Climbing,
+}
+
+var player_movement := PlayerMovement.Idle
+
+func can_climb() -> bool: #{
 	if !ray_cast_front_top.is_colliding() || !ray_cast_front_bottom.is_colliding():
 		return false
 	var top_dist = ray_cast_front_top.global_transform.origin.distance_to(ray_cast_front_top.get_collision_point())
@@ -30,8 +42,9 @@ func can_climb() -> bool:
 	if top_dist < 1.5 && bottom_dist < 1.5:
 		return true
 	return false
+#}
 
-func climb_slope() -> float:
+func climb_slope() -> float: #{
 	if !ray_cast_front_top.is_colliding() || !ray_cast_front_bottom.is_colliding():
 		return 0.0
 	var top_dist = ray_cast_front_top.global_transform.origin.distance_to(ray_cast_front_top.get_collision_point())
@@ -42,37 +55,52 @@ func climb_slope() -> float:
 	if top_dist < bottom_dist:
 		angle = -angle
 	return angle
+#}
+
+
+func get_surface_rotation() -> Transform3D:
+	var up = Vector3.UP
+	var wall_normal = ray_cast_front_top.get_collision_normal()
+	var wall_tangent = wall_normal.cross(up).normalized()
+	var wall_up = wall_normal.cross(wall_tangent).normalized()
 	
+	return Transform3D(Basis(wall_tangent, wall_up, wall_normal), Vector3.ZERO)
 
+func process_climbing(delta: float) -> void: #{
+	if Input.is_action_just_pressed("crouch"):
+		player_movement = PlayerMovement.Idle
+		return
+	var input_dir := Input.get_vector("right", "left", "forward", "backward")
+	var direction := (Vector3(input_dir.x, input_dir.y, 0)).normalized()
+	if direction:
+		velocity = (get_surface_rotation() * direction) * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+	var to_wall := -get_surface_rotation().basis.z
+	to_wall.y = 0
+	velocity += to_wall * 5
+	move_and_slide()
+#}
 
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
-		camera.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90.0), deg_to_rad(60.0))
-	
-	if event.is_action_pressed("ui_cancel") && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	if event is InputEventMouseButton && Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-
-func _physics_process(delta: float) -> void:
+func process_movement(delta: float) -> void: #{
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() && player_movement != PlayerMovement.Climbing:
 		velocity += get_gravity() * delta
+		if velocity.y < 0:
+			player_movement = PlayerMovement.Falling
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
+		player_movement = PlayerMovement.Jumping
 	
 	if Input.is_action_pressed("run"):
 		speed = run_speed
 		camera.fov = base_fov + run_fov_change
+		if is_on_floor():
+			player_movement = PlayerMovement.Running
 	else:
 		speed = walk_speed
 		camera.fov = base_fov
@@ -84,9 +112,47 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
+		if player_movement != PlayerMovement.Running && is_on_floor():
+			player_movement = PlayerMovement.Walking
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
+	if velocity.is_zero_approx() && player_movement != PlayerMovement.Climbing:
+		player_movement = PlayerMovement.Idle
+	
 	move_and_slide()
-	print(climb_slope())
+#}
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+
+
+func _input(event: InputEvent) -> void: #{
+	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		camera.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90.0), deg_to_rad(60.0))
+	
+	if event.is_action_pressed("ui_cancel") && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event is InputEventMouseButton && Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event.is_action_pressed("climb") && climb_slope() > 45:
+		player_movement = PlayerMovement.Climbing
+		velocity = Vector3.ZERO
+#}
+
+func _physics_process(delta: float) -> void: #{
+	if player_movement == PlayerMovement.Climbing:
+		if climb_slope() <= 45:
+			player_movement = PlayerMovement.Idle
+		else:
+			process_climbing(delta)
+	else:
+		process_movement(delta)
+	#print(climb_slope())
+	#print(player_movement)
+#}
