d627a2b701aa5d9d103c22e0833280bda98d2685
hinrikfp <hinrikfp06@gmail.com>
2025-02-19 23:08:46 +0000

    basic day time progression

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,14 +1,21 @@
 extends Node
 
-@export var day_length_s = 600;
+@onready var directional_light_3d: DirectionalLight3D = $DirectionalLight3D
+@onready var world_environment: WorldEnvironment = $WorldEnvironment
 
-var start_time_ms;
+@export var day_length_s: float = 600;
+
+var start_time_ms: float;
+var initial_light_energy: float;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_time_ms = Time.get_ticks_msec();
+	initial_light_energy = directional_light_3d.light_energy
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	directional_light_3d.rotate_x(deg_to_rad((360.0 / day_length_s) * delta))
+	directional_light_3d.light_energy = clamp(initial_light_energy * ((90-abs(rad_to_deg(directional_light_3d.rotation.z))) / 90) + 0.2, 0.0, 0.8)
+	
